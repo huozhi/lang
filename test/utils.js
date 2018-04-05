475374e0b9b09324025c3361898b828119802b6d
huozhi <inbox@huozhi.im>
2018-04-05 16:06:10 +0800

    prettify log

--- test/utils.js
+++ test/utils.js
@@ -3,4 +3,15 @@ const path = require('path')
 
 const readFile = (filename) => fs.readFileSync(path.resolve(__dirname, filename), 'utf8')
 
-module.exports = {readFile}
+const logEmittedCode = (emitted) => {
+  for (let i = 0; i < emitted.length; i++) {
+    const command = emitted[i].toString()
+    let text = command
+    if (command === 'IMM') {
+      text += (' ' + emitted[++i].toString())
+    }
+    console.log(text)
+  }
+}
+
+module.exports = {readFile, logEmittedCode}
